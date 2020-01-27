f63c30b8a7722f364c80bdbf1ecb51d178e3d931
Marian Kucharski <marian.kucharski@itelo.pl>
2020-01-27 11:30:53 +0100

    forget to format style...

--- simpleReporter.js
+++ simpleReporter.js
@@ -1,8 +1,7 @@
 const fs = require("fs");
 const { exec, execSync } = require("child_process");
 const log = fs.readFileSync("./testLog.txt", "UTF-8");
-const slackHook =
-  "***";
+const slackHook = "***";
 
 const report = logFile => {
   const timeNumber = logFile.search("Time:");
@@ -19,8 +18,9 @@ const report = logFile => {
 
   return new Promise((resolve, reject) => {
     if (logFile.indexOf("FAILURES!!!") === -1) {
-
-      exec(`curl -X POST -H 'Content-type: application/json' --data '{"text":":white_check_mark: --- All tests completed successfully --- :white_check_mark:\n\n:clipboard:*Tests performed:*\n\n${performedSummary}\n\n:hourglass:*${testTime}* seconds\n\n:arrow_down: For more info see the log file :arrow_down:"}' ${slackHook}`);
+      exec(
+        `curl -X POST -H 'Content-type: application/json' --data '{"text":":white_check_mark: --- All tests completed successfully --- :white_check_mark:\n\n:clipboard:*Tests performed:*\n\n${performedSummary}\n\n:hourglass:*${testTime}* seconds\n\n:arrow_down: For more info see the log file :arrow_down:"}' ${slackHook}`
+      );
 
       resolve(`--- All tests completed successfully ---\n${testStringInfo}`);
     } else {
