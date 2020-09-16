00771cc775a52f6d79be96d541b837f0b9fa24ff
Marian Kucharski <marian@lifeinmobile.com>
2020-09-16 10:08:09 +0200

    working draft for directory indexing

--- newReporter.js
+++ newReporter.js
@@ -1,54 +1,86 @@
 const { WebClient } = require("@slack/web-api");
+const fs = require("fs");
 
 // An access token for gitlab CI safe token:
 //const token = process.env.SLACK_TOKEN;
 
 const token = "xoxb-221114625218-1350157923462-3Rubez2y2IH84Sez51d3cNNx";
 const channel = "C01A7EY7A9G";
+const logInfo = ["./testLogs/fLog.txt", "UTF-8"];
 
 class Reporter {
-  constructor(token, channel) {
+  constructor([path, coding], token, channel, testsPerformed) {
+    this.result = {};
+    this.testsPerformed = testsPerformed;
     this.authToken = token;
     this.web = new WebClient(this.authToken);
     this.channel = channel;
+    this.path = path;
+    this.coding = coding;
+    this.file = fs.readFileSync(this.path, this.coding);
+    this.logArray = this.file.split(/\n/);
+    this.failedUrls = [];
   }
+
+  getResult = () => {
+    if (this.file.includes("CHECK")) {
+      this.listFails = this.logArray.forEach((element) => {
+        element.includes("CHECK")
+          ? this.failedUrls.push(
+              element.replace(` - \u001b[35m CHECK NEEDED! \u001b[0m`, "\n")
+            )
+          : [];
+      });
+      return (this.result = {
+        title: "Test failed!",
+        status: "fail",
+        color: "#DC143C",
+        description: "All provided URL's are safe.",
+      });
+    } else {
+      return (this.result = {
+        title: "Test success!",
+        status: "succ",
+        color: "#048a04",
+        description:
+          "There are some URL's that have catalog / file indexing on.",
+      });
+    }
+  };
+
   postResult = async () => {
     try {
       await this.web.chat.postMessage({
-        text: `:iphone: *Test performed*\n.`,
+        text: `:package: *Directory / file indexing test:*\n.`,
         icon_emoji: ":clipboard:",
         attachments: [
           {
-            color: "#048a04",
+            color: this.result.color,
             fields: [
               {
                 title: ":question:*Test outcome:*",
-                value: `*Fail or not fail*\n---`,
+                value: `--- *${this.result.title}* ---\n`,
                 short: true,
               },
               {
                 title: ":card_file_box:*Type:*",
-                value: `Test type\n---`,
-                short: true,
-              },
-              {
-                title: ":clipboard:*Tests performed:*",
-                value: `List of tests`,
+                value: `Standard everyday check.\n.\n`,
                 short: true,
               },
               {
                 title: ":hourglass:*Time:*",
-                value: `120 minutes 50 seconds`,
+                value: `<10 sec\n.`,
                 short: true,
               },
               {
-                title:
-                  ":arrow_up_down: For more info see the log file :arrow_up_down:",
+                title: ":bar_chart:For more info see the log: ",
+                value:
+                  "https://gitlab.lim.bz/dev/directory-indexing-check/-/jobs",
                 short: false,
               },
               {
-                title: `Failed tests`,
-                value: `- list of tests failed`,
+                title: `Failed URLs:`,
+                value: `${this.failedUrls}`,
                 short: false,
               },
             ],
@@ -64,5 +96,6 @@ class Reporter {
   };
 }
 
-const reporter = new Reporter(token, channel);
+const reporter = new Reporter(logInfo, token, channel);
+reporter.getResult();
 reporter.postResult();
