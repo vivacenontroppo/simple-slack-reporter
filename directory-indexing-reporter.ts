baa3e12b4747cff927bc0eb03c32f8b14b98dce8
Marian Kucharski <marian@lifeinmobile.com>
2020-09-16 16:07:26 +0200

    finished with directory indexing implementation

---
+++ directory-indexing-reporter.ts
@@ -0,0 +1,129 @@
+const { WebClient } = require("@slack/web-api");
+const fs = require("fs");
+const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+interface resultT {
+  title: string;
+  failed: boolean;
+  color: string;
+  description: string;
+}
+
+// An access token for gitlab CI safe token:
+// const token = process.env.SLACK_TOKEN;
+
+export class Reporter {
+  channel: string;
+  logInfo: string[];
+  authToken: string;
+  file: string;
+  logArray: string[];
+  failedUrls: string[];
+  result: resultT;
+  slackApp: any;
+
+  constructor() {
+    this.channel = config.slack.channel;
+    this.logInfo = config.slack.logInfo;
+    this.authToken = config.slack.token;
+    this.file = fs.readFileSync(this.logInfo[0], this.logInfo[1]);
+    this.logArray = this.file.split(/\n/);
+    this.slackApp = new WebClient(this.authToken);
+    this.failedUrls = [];
+  }
+
+  getResult = async (): Promise<resultT> => {
+    this.file.includes("CHECK")
+      ? this.logArray.forEach((el) => {
+          if (el.includes("CHECK")) {
+            this.failedUrls.push(
+              el.replace(" - \u001b[35m CHECK NEEDED! \u001b[0m", "\n")
+            );
+          }
+        })
+      : [];
+    if (this.file.includes("CHECK")) {
+      return (this.result = {
+        title: "Test failed!",
+        failed: true,
+        color: "#DC143C",
+        description:
+          "There are some URL's that have catalog / file indexing on.",
+      });
+    } else if (!this.file.includes("CHECK") && !this.file.includes("OK")) {
+      return (this.result = {
+        title: "Process crashed, test run failed!",
+        failed: true,
+        color: "#eb349e",
+        description: "There is no meaningful result in log file!",
+      });
+    } else {
+      return (this.result = {
+        title: "Test success!",
+        failed: false,
+        color: "#048a04",
+        description: "All provided URL's are safe.",
+      });
+    }
+  };
+
+  postResult = async (res: resultT): Promise<void> => {
+    try {
+      await this.slackApp.chat.postMessage({
+        text: `:package: *Directory / file indexing test:*\n.`,
+        icon_emoji: ":clipboard:",
+        attachments: [
+          {
+            color: res.color,
+            fields: [
+              {
+                title: ":question:*Test outcome:*",
+                value: `--- *${res.title}* ---\n`,
+                short: true,
+              },
+              {
+                title: ":memo:*Description:*",
+                value: `${res.description}.\n.\n`,
+                short: true,
+              },
+              {
+                title: ":hourglass:*Time:*",
+                value: `<10 sec\n.`,
+                short: true,
+              },
+              {
+                title: ":bar_chart:For more info see the log: ",
+                value: config.slack.pipelinesUrl,
+                short: false,
+              },
+              {
+                title: `Failed URLs:`,
+                value: `${this.failedUrls}`,
+                short: false,
+              },
+            ],
+          },
+        ],
+        channel: this.channel,
+      });
+
+      console.log("Message posted!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  resolveTest(): void {
+    if (this.result.failed) {
+      throw new Error(config.slack.errorMsg);
+    } else {
+      console.log(config.slack.succMsg);
+    }
+  }
+}
+
+const reporter = new Reporter();
+
+reporter
+  .getResult()
+  .then((res) => reporter.postResult(res))
+  .finally(() => reporter.resolveTest());
